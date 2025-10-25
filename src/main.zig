ff52a59ab25504f3467640d5da9b0d857600ce56
Ali Aryani <lenesis@proton.me>
2025-10-25 23:23:53 +0330

    declare print_pascal; test row

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,12 @@ var stdout_buffer:[1024]u8 =undefined;
 var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
+fn print_pascal(n:u8) !void {
+    for (1..n+1) |row| {
+        std.debug.print("row: {d}\n", .{row});
+    }
+}
+
 pub fn main() !void {
+    try print_pascal(10);
 }
