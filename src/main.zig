e0b950298892d73b535414169ef12bd0b11c970e
Ali Aryani <lenesis@proton.me>
2025-10-25 23:40:43 +0330

    alignment

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,12 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 fn print_pascal(n:u8) !void {
+    var align_space = n;
     for (1..n+1) |row| {
+        for (0..align_space) |_| {
+            try stdout.print(" ", .{});
+        }
+        align_space -= 1;
         var c:u64 = 1;
         for (1..row+1) |i| {
             try stdout.print("{d} ", .{c});
